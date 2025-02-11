e68d69fec6de5d66d950023e5b72871a75cf7263
Vipul Avinash <vipulavinash123@gmail.com>
2025-02-11 16:00:10 +0530

    Sample Commit

---
+++ backend/db/config.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery',true);
+mongoose.connect("mongodb+srv://Vipul:9912736854@mernapp.txma5.mongodb.net/?retryWrites=true&w=majority&appName=MERNapp");
+
